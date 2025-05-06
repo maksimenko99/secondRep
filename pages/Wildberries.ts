71039e55cec35d2aea3786511c5abc1cab31dac8
Maxim <relax8.99@mail.ru>
2025-05-06 19:36:16 +0300

    updateTest

--- pages/Wildberries.ts
+++ pages/Wildberries.ts
@@ -9,7 +9,7 @@ export class Wildberries {
 
     async goTo(){
         await this.page.goto('https://www.wildberries.ru/')
-        await this.page.waitForLoadState('networkidle')
+        await this.page.waitForTimeout(2000)
     }
 
     async fillSearch(){
